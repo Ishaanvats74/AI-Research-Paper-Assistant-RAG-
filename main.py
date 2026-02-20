cae22adaa22bf0367a9ec77784a76f83d09822aa
Ishaan vats <ishaanvats74@gmail.com>
2026-02-20 15:13:43 +0530

    project diagram had been made

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
